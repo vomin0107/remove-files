c5e1f295833a8f96489b05b622b85a252ce5e319
MW Kim <vomin0107@gmail.com>
2021-03-03 12:00:23 +0900

    rename

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 import os
 
 # 파일이 있는 폴더 경로
-path = r'C:\Users\hong0\OneDrive - 주식회사 단후이\문서\LI\All_document\AI_고부과_과제\Source\annotation_type'
+path = r'C:\Users\dir'
 
 os_files = os.listdir(path)
 print(os_files)
 for dir in os_files:
-    file_name = path + '/' + dir + '/README.roboflow.txt'
+    file_name = path + '/' + dir + '/file_name.txt'
     if os.path.isfile(file_name):
         os.remove(file_name)
         #os.rmdir(path, option) # remove directory
